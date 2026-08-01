b3c73a9900ac4eebeda2206ce358287b40c970cc
Simomik <svputrov@gmail.com>
2026-08-01 12:18:43 +0300

    костыльно, но дверь теперь открываясь влияет на навигацию
    Это пизда.
    Я три часа вчера ебался с тем чтобы сделать так чтобы Tilemap сам определял когда на нём непроходимый obstacle и включал или выключал навигацию на нужном тайле, но я не нашёл способа форсировать обновления тайлмапы чтобы она проверяла нужные тайлы при открытии или закрытии двери.
    Поэтому я просто ебанул стандартный тайл пути, не отличающийся от его брата ничем кроме отсутсвия на нём сетки навигации, и теперь когда дверь открывается или закрывается она меняет тайл под собой на тайл с навигацией и без соответсвенно.
    Теперь класс Obstacle бесполезен, у нас есть рандомный скрипт для tilemap который нихуя не делает, а дверь ахуенно работает через какой-то костыль.
    Единственный минус -- я не ебу как сделать теперь дверь на тайлах кроме стандартного, но давайте честно, нам эта возможность не особо-то и нужна.

--- Assets/scripts/Obstacle.gd
+++ Assets/scripts/Obstacle.gd
@@ -8,3 +8,4 @@ var cell: Vector2i
 func _ready() -> void:
 	TileMap_I_Am_Standing_On = get_tree().get_first_node_in_group("TileMap")
 	cell = TileMap_I_Am_Standing_On.local_to_map(position)
+	TileMap_I_Am_Standing_On.set_cell(cell,0, Vector2i(0,!is_passable))
--- Assets/scripts/door.gd
+++ Assets/scripts/door.gd
@@ -4,21 +4,20 @@ func open():
 	is_passable = true
 	$CollisionShape2D.disabled = true
 	$Closed.visible = false
-
+	TileMap_I_Am_Standing_On.set_cell(cell,0, Vector2i(0,!is_passable))
+	
 func close():
 	is_passable = false
 	$CollisionShape2D.disabled = false
 	$Closed.visible = true
-
+	TileMap_I_Am_Standing_On.set_cell(cell,0, Vector2i(0,!is_passable))
+	
 func toggle():
 	is_passable = !is_passable
 	$CollisionShape2D.disabled = !$CollisionShape2D.disabled
 	$Closed.visible = !$Closed.visible
-	
-func _ready():
-	toggle()
+	TileMap_I_Am_Standing_On.set_cell(cell,0, Vector2i(0,!is_passable))
 	
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("use_item"):
 		toggle()
-	
