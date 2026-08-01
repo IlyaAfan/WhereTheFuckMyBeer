e468e4fc0edf03d4926d0692a751550d445eeb7f
Simomik <svputrov@gmail.com>
2026-08-01 13:32:16 +0300

    починил пару багов
    починил баг где почему-то противник ебал игрока
    починил странность отображения уровней в менюшке

--- Assets/scripts/door.gd
+++ Assets/scripts/door.gd
@@ -2,13 +2,15 @@ extends Obstacle
 
 func open():
 	is_passable = true
-	$CollisionShape2D.disabled = true
+	collision_layer = 0
+	collision_mask = 0
 	$Closed.visible = false
 	TileMap_I_Am_Standing_On.set_cell(cell,0, Vector2i(0,!is_passable))
 	
 func close():
 	is_passable = false
-	$CollisionShape2D.disabled = false
+	collision_layer = 1
+	collision_mask = 1
 	$Closed.visible = true
 	TileMap_I_Am_Standing_On.set_cell(cell,0, Vector2i(0,!is_passable))
 	
