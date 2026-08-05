920469387677943633dbe903008baa9a5a7d812b
Dizgvar <dchaikin@sfedu.ru>
2026-08-05 21:24:03 +0300

    Долбанное казино
    Сделал окошко вылазящее у казино и крутилку в нем

---
+++ Assets/scripts/window_kazino.gd
@@ -0,0 +1,38 @@
+extends Popup
+
+var active = false
+var ivent:int
+var twist: bool
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Label.text = "Баланc золота: " + str(ConfigOption.gold)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if active and Input.is_action_just_pressed("use_item"):
+		_machine_random()
+	if twist:
+		$Kazino.rotation += 0.1*PI
+
+		
+func _on_button_pressed() -> void:
+	_machine_random()
+		
+func _machine_random():
+	ConfigOption.load_game_stat()
+	ConfigOption.gold -= 1
+	ConfigOption.save_game_stat()
+	$Label.text = "Баланс золота: " + str(ConfigOption.gold)
+	ivent = randi_range(0,7)
+	twist = true
+	await get_tree().create_timer(2.98).timeout
+	print($Kazino.rotation/PI)
+	twist = false
+	active = true
+
+
+func _on_button_pressed_not_open() -> void:
+	$".".visible = false
+	$"../../Player".speed = 60
